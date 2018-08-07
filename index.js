c955a60b6e9879621a216f2e4384196455db58ba
pranav prashant <pranav0073@gmail.com>
2018-08-07 18:43:25 +0530

    added codemirror

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+var express = require('express');
 var app = require('express')();
 var fs = require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var process = require('process');
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/codemirror-5.39.2'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,6 +14,16 @@ app.get('/', function(req, res){
 app.get('/test', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/about', function(req, res){
+  fs.readdir(__dirname+'/trackFiles',(err,files)=>{
+    console.log(err);
+    console.log(files);
+    res.render(__dirname + '/pages/index',{files:files});
+  })
+
+});
+
 app.get('/test/*', function(req, res){
   //create a folder
 
@@ -18,19 +31,25 @@ app.get('/test/*', function(req, res){
   console.log(param);
   process.umask = 0;
   fs.mkdir(__dirname+`/test-${param}`,0777,(err)=>{
-    if(err)
+    if(err){
       if(err.code === 'EEXIST'){
-        fs.readdir(__dirname+`/test-${param}`,'utf8',(err,files)=>{
+        fs.readdir(__dirname+`/test-${param}`,(err,files)=>{
+          console.log(err);
           console.log(files);
-        });
+          res.render(__dirname + '/pages/index',{files:files});
+        })
       }
+    }
+    else
+    res.sendFile(__dirname + '/index.html');
   });
-  res.sendFile(__dirname + '/index.html');
+
 });
 
 io.on('connection', function(socket){
+  //console.log(socket.handshake.);
   socket.on('chat message', function(msg){
-    
+
     console.log(io.listenerCount('chat message'));
     //io.emit('chat message'+msg.loc, msg.val);
   });
@@ -41,6 +60,8 @@ io.on('connection', function(socket){
     console.log(msg.val);
     console.log(io.listenerCount('chat message'));
     if(msg.val==="trigger.txt"){
+      console.log('pranav');
+      //console.log(`/test-${param}`);
       fs.readFile(__dirname+'/tirgger.txt','utf8',(err,data)=>{
         if(err)
           console.log(err);
