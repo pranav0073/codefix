5fec9149c59e3d674929586dd8a172cc8055bba0
Nisarga Murthy <nmurthy@akamai.com>
2018-08-09 11:07:28 +0530

    File upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var process = require('process');
+var path = require('path');
+var formidable = require('formidable');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -49,6 +51,38 @@ app.get('/project/*', function(req, res){
     }
 
   });
+
+});
+
+app.post('/upload', function(req, res){
+
+console.log('in upload');
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+  // store all uploads in the /uploads directory
+  form.uploadDir = __dirname+ '/test-pro';
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
 
 });
 
